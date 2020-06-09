e002936f6cc081a5b1ebba98ae3f0805b05b0c04
Diego N <54142512+dnmaster2@users.noreply.github.com>
2020-06-09 03:07:11 -0300

    A-star instalado + movimentação com o cursor
    1/7 itens

---
+++ Assets/PlayerPathScript.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class PlayerPathScript : MonoBehaviour
+{
+    public Transform targetPosition;
+
+    private Seeker seeker;
+    private CharacterController controller;
+    public Path path;
+
+    Camera cam;
+    public GameObject cursor;
+    public float floorHeight;
+    bool moving;
+
+    public float speed = 2;
+    public float nextWaypointDistance = 3;
+    private int currentWaypoint = 0;
+    public bool reachedEndOfPath;
+
+    public void Start()
+    {
+        cam = Camera.main;
+        seeker = GetComponent<Seeker>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    public void OnPathComplete(Path p)
+    {
+        Debug.Log("Caminho calculado, erros: " + p.error);
+
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    public void NewPath(Vector3 newTarget)
+    {
+        seeker.StartPath(transform.position, newTarget, OnPathComplete);
+    }
+
+    public void Update()
+    {
+        cursor.SetActive(moving);
+        if (Input.GetMouseButton(0))
+        {
+            if (!moving)
+            {
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.CompareTag("Player"))
+                    {
+                        moving = true;
+                        cursor.transform.position = hit.point;
+                    }
+                }
+            }
+            if (moving)
+            {
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    cursor.transform.position = hit.point;
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            moving = false;
+            NewPath(cursor.transform.position);
+        }
+
+        if (path == null)
+        {
+            return;
+        }
+
+        reachedEndOfPath = false;
+        float distanceToWaypoint;
+        while (true)
+        {
+            distanceToWaypoint = Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]);
+            if (distanceToWaypoint < nextWaypointDistance)
+            {
+                if (currentWaypoint + 1 < path.vectorPath.Count)
+                {
+                    currentWaypoint++;
+                }
+                else
+                {
+                    reachedEndOfPath = true;
+                    break;
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        var speedFactor = reachedEndOfPath ? Mathf.Sqrt(distanceToWaypoint / nextWaypointDistance) : 1f;
+        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+        Vector3 velocity = dir * speed * speedFactor;
+        controller.SimpleMove(velocity);
+    }
+}
