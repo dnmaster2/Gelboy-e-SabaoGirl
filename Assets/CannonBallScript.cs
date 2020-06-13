8197a015f3268764ba19708cce397afc378755c2
Diego N <54142512+dnmaster2@users.noreply.github.com>
2020-06-12 21:15:27 -0300

    Canhão implementado

---
+++ Assets/CannonBallScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBallScript : MonoBehaviour
+{
+    public int damage;
+    private void Awake()
+    {
+        Destroy(gameObject, 3);
+        GetComponent<Rigidbody>().velocity = transform.forward * 20;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<Attributes>().health -= damage;
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/CursorControlScript.cs
+++ Assets/Scripts/CursorControlScript.cs
@@ -6,6 +6,7 @@ public class CursorControlScript : MonoBehaviour
 {
     Camera cam;
     public GameObject cursor;
+    public GameObject cannonShoot, cannonCursor;
     public float buffCombatTarget;
     GameObject target;
     bool moving;
@@ -22,6 +23,35 @@ public class CursorControlScript : MonoBehaviour
 
     private void Update()
     {
+        cannonCursor.SetActive(BuffManager.instance.cannonIsActive);
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (BuffManager.instance.cannonIsActive)
+            {
+                var cannonLookPos = cannonCursor.transform.position - transform.position;
+                cannonLookPos.y = 0;
+                var cannonRotation = Quaternion.LookRotation(cannonLookPos);
+                GameObject cannonBall = Instantiate(cannonShoot, transform.position + transform.forward * 2, cannonRotation, transform.parent);
+            }
+        }
+
+        if (BuffManager.instance.cannonIsActive)
+        {
+            cannonCursor.SetActive(true);
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                cannonCursor.transform.position = hit.point;
+            }
+
+            var cannonLookPos = cannonCursor.transform.position - transform.position;
+            cannonLookPos.y = 0;
+            var cannonRotation = Quaternion.LookRotation(cannonLookPos);
+            transform.rotation = Quaternion.Slerp(transform.rotation, cannonRotation, Time.deltaTime * 10);
+            return;
+        }
+
         var lookPos = cursor.transform.position - transform.position;
         lookPos.y = 0;
         var rotation = Quaternion.LookRotation(lookPos);
@@ -66,12 +96,13 @@ public class CursorControlScript : MonoBehaviour
 
         if (Input.GetMouseButtonUp(0))
         {
-            moving = false;
-            if (!combat)
-            {                
+            if (!combat && moving)
+            {
                 pathfinding.NewPath(cursor.transform.position);
+                moving = false;
                 return;
             }
+
             if (combat)
             {
                 if (target)
@@ -79,7 +110,9 @@ public class CursorControlScript : MonoBehaviour
                     combatScript.DashTarget(target, target.tag);
                     pathfinding.NewPath(target.transform.position + transform.forward * buffCombatTarget);
                     pathfinding.speed *= 4;
-                }              
+                    moving = false;
+                }
+                return;
             }
         }
     }
