8de4ecad2e6216d6e094fb0250c17afa7072f6f9
Diego N <54142512+dnmaster2@users.noreply.github.com>
2020-06-18 23:48:20 -0300

    Corrigido gameManager + Adicionado AudioManager em todas as cenas

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -30,6 +30,10 @@ public class GameManager : MonoBehaviour
     private void Start()
     {
         Time.timeScale = 1;
+        if (SceneManager.GetActiveScene().name == "Fase1" || SceneManager.GetActiveScene().name == "Fase2" || SceneManager.GetActiveScene().name == "Fase3" || SceneManager.GetActiveScene().name == "Fase4")
+        {
+            FindObjectOfType<AudioManager>().Play("Level");
+        }
     }
 
     #endregion
@@ -37,14 +41,6 @@ public class GameManager : MonoBehaviour
 
 
     #region Custom Callbacks
-
-    void Start()
-    {
-      if(SceneManager.GetActiveScene().name == "Fase1" || SceneManager.GetActiveScene().name == "Fase2" || SceneManager.GetActiveScene().name == "Fase3" || SceneManager.GetActiveScene().name == "Fase4" )
-      {
-        FindObjectOfType<AudioManager>().Play("Level");
-      }
-    }
 
     public void EndLevel(int points)
     {
