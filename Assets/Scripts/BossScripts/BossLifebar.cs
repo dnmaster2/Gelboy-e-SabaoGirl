1709cc4632df5aed9cb84992ef01253b82ae3229
Nathan Morsch <54150372+INotCamper@users.noreply.github.com>
2020-06-19 17:01:28 -0300

    Possivel problema com numero do lvl, barra de vida do boss, sons dos botoes do menu, botões novos nas configs

--- Assets/Scripts/BossScripts/BossLifebar.cs
+++ Assets/Scripts/BossScripts/BossLifebar.cs
@@ -1,22 +1,20 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class DiegeticLifeBar : MonoBehaviour
+public class BossLifebar : MonoBehaviour
 {
     [SerializeField] Attributes enemyAttributes;
     [SerializeField] Slider healthBar;
     [SerializeField] Image fillHealthBar;
     [SerializeField] Gradient colorBar;
-    [SerializeField] Transform cameraPos;
+
     private void Awake()
     {
-        enemyAttributes = transform.root.gameObject.GetComponent<Attributes>();
         healthBar.maxValue = enemyAttributes.health;
-        cameraPos = Camera.main.transform;
     }
+
     private void Update()
     {
-        transform.LookAt(cameraPos);
         healthBar.value = enemyAttributes.health;
         fillHealthBar.color = colorBar.Evaluate(healthBar.normalizedValue);
     }
