cef2a92672a808b37d2d527bb4d8cb6993a38495
Diego N <54142512+dnmaster2@users.noreply.github.com>
2020-06-19 02:54:31 -0300

    Sons adicionados + inicio cena boss

--- Assets/Scripts/BossBehaviour.cs
+++ Assets/Scripts/BossBehaviour.cs
@@ -4,37 +4,23 @@ using UnityEngine;
 
 public class BossBehaviour : MonoBehaviour
 {
-    public ParticleSystem fireParticles;
-    public GameObject triggerFire;
-    public EnemyIA enemyIA;
-    public bool attacking, animAttack;
-    public float rangeRadius, preparationTime, attackTime, burnTime;
-    public AnimationClip attackAnimation;
-    public AnimationClip raiseAnimation;
+    public bool fire, melee;
+    Animator anim;
 
-    void Update()
+    private void Awake()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    IEnumerator FlameThrowerRoutine()
+    private void Update()
     {
-        attacking = true;
-        print("preparando");
-        StartCoroutine(enemyIA.Stun(attackTime + preparationTime));
-        animAttack = true;
-        yield return new WaitForSeconds(preparationTime + 1f);
-        print("ataque");
-        fireParticles.Play();
-        triggerFire.SetActive(true);
-        yield return new WaitForSeconds(attackTime - 1f);
-        print("fim do ataque");
-        fireParticles.Pause();
-        fireParticles.Clear();
-        triggerFire.SetActive(false);
-        animAttack = false;
-        yield return new WaitForSeconds(2f);
-        print("reset");
-        attacking = false;
+        anim.SetBool("Combo", melee);
+        anim.SetBool("Fire", fire);
     }
+
+    IEnumerator SpitFire()
+    {
+        yield return new WaitForSeconds(.5f);
+    }
+
 }
